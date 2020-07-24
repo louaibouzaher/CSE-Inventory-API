17465d8bd1dcb03ed562eb39cb40b4df7a34b1ef
louaibouzaher <jm_bouzaher@esi.dz>
2020-07-25 00:57:11 +0200

    Added description to itemModel and Joi to itemRoutes

--- app/Models/ItemModel.js
+++ app/Models/ItemModel.js
@@ -9,6 +9,7 @@ const itemHistorySchema = mongoose.Schema({
 
 const ItemSchema = mongoose.Schema({
   objectName: { type: String, required: true },
+  objectDescription: { type: String },
   objectImage: {  type: String, required: true },
   objectOccurence: { type: Number, required: true, default: 1},
   objectState: { type: String, required: true }, // Broken, Mobile, Immobile 
--- app/Routes/itemsRoutes.js
+++ app/Routes/itemsRoutes.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const multerConfig = require("../Configs/multerConfig");
-const fs = require('fs')
+const fs = require("fs");
 const upload = multer({
   storage: multerConfig.storage,
   fileFilter: multerConfig.fileFilter,
 });
-
+const Joi = require("joi");
 const Item = require("../Models/ItemModel");
-
 
 // GET Request to all Items
 router.get("/all", async (req, res) => {
@@ -57,31 +56,54 @@ router.get("/broken", async (req, res) => {
 
 // POST Request to Add a new Item
 router.post("/add", upload.single("objectImage"), async (req, res, next) => {
-  console.log(`Hello ${req.file.path}`)
-  const newItem = new Item({
+  const itemSchema = Joi.object().keys({
+    objectName: Joi.string().required(),
+    objectDescription: Joi.string(),
+    objectImage: Joi.string().required(),
+    objectState: Joi.string().required(), // Broken, Mobile, Immobile
+  });
+  const body = {
     objectName: req.body.objectName,
+    objectDescription: req.body.objectDescription,
     objectImage: req.file.path,
     objectState: req.body.objectState, // Broken, Mobile, Immobile
-  });
-  
-  await newItem 
-    .save()
-    .then(
-      res.status(201).json({
-        newItem,
-        message: "Item Added Successfully",
-      })
-    )
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+  };
+  const result = itemSchema.validate(body);
+
+  const { error } = result;
+  const valid = error == null;
+
+  if (!valid) {
+    res.status(422).json({
+      message: "Invalid request",
+      data: body,
     });
+  }
+
+  try {
+    const newItem = new Item({
+      objectName: req.body.objectName,
+      objectDescription: req.body.objectDescription,
+      objectImage: req.file.path,
+      objectState: req.body.objectState, // Broken, Mobile, Immobile
+    });
+
+    await newItem
+      .save()
+      .then(
+        res.status(201).json({
+          newItem,
+          message: "Item Added Successfully",
+        })
+      )
+     
+  } catch (err) {
+    res.status(500);
+  }
   next();
 });
 
-// Get Request to get a new Item
+// Get Request to get an Item specified by Id
 router.get("/get/:id", async (req, res) => {
   try {
     let item = await Item.findById(req.params.id);
@@ -97,24 +119,23 @@ router.get("/get/:id", async (req, res) => {
       message: "Item does not exist !",
     });
   }
-})
+});
 
 // DELETE Request to remove an item
 router.delete("/delete/:id", async (req, res) => {
   try {
-    const deletedItem = await Item.findById(req.params.id)
+    const deletedItem = await Item.findById(req.params.id);
     if (deletedItem) {
-      fs.unlinkSync(`./${deletedItem.objectImage}`)
-      await deletedItem.delete()
-      res.send("Object removed succefully")
+      fs.unlinkSync(`./${deletedItem.objectImage}`);
+      await deletedItem.delete();
+      res.send("Object removed succefully");
     } else {
       res.send("Item does not exist");
     }
   } catch (err) {
-    console.log(err)
-    res.send(err)
+    console.log(err);
+    res.send(err);
   }
 });
-
 
 module.exports = router;
