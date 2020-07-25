ddccc073e8507ac1e0ff691698dbc28edc958c46
louaibouzaher <jm_bouzaher@esi.dz>
2020-07-25 06:50:44 +0200

    Added actionDate to Action Model

--- app/Models/ActionModel.js
+++ app/Models/ActionModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const actionSchema = mongoose.Schema({
+  actionDate: { type: Date, default: Date.now },
   reservationId: { type: mongoose.Schema.Types.ObjectId, ref: "Reservation" },
   reportId: { type: mongoose.Schema.Types.ObjectId, ref: "Report" },
   lostObjectId: { type: mongoose.Schema.Types.ObjectId, ref: "LostObject" },
