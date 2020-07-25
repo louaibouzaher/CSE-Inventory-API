892b53f0d7cddfbccb41e48316b1cf85f09e8471
louaibouzaher <jm_bouzaher@esi.dz>
2020-07-25 06:43:08 +0200

    Add patch route to Action Routes

--- app/Routes/actionsRoutes.js
+++ app/Routes/actionsRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Action = require("../Models/ActionModel");
+const { read } = require("fs");
 
 // GET Request to all Actions
 router.get("/all", async (req, res) => {
@@ -17,6 +18,18 @@ router.get("/all", async (req, res) => {
   }
 });
 
-
+router.patch("/edit/:id", async (req, res) => {
+  const targetAction = Action.findById(req.params.id);
+  try {
+    await targetAction
+      .update({
+        done: req.body.done,
+      })
+      .then(res.sendStatus(200));
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
