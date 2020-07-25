644e55348002aebe79f3891bb35c11afa1afcfcd
louaibouzaher <jm_bouzaher@esi.dz>
2020-07-25 07:12:16 +0200

    Added takenBy User Route

--- app/Models/ItemModel.js
+++ app/Models/ItemModel.js
@@ -6,8 +6,7 @@ const ItemSchema = mongoose.Schema({
   objectImage: { type: String, required: true },
   objectOccurence: { type: Number, required: true, default: 1 },
   objectState: { type: String, required: true }, // Broken, Mobile, Immobile
-  itemDispo: { type: String, required: true },
-});
+ });
 
 const Item = mongoose.model("Item", ItemSchema);
 module.exports = Item;
--- app/Routes/userRoutes.js
+++ app/Routes/userRoutes.js
@@ -1,170 +1,192 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const Joi = require('joi')
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const Reservation = require("../Models/ReservationModel");
 const User = require("../Models/UserModel");
-const auth = require("../Middleware/auth")
+const auth = require("../Middleware/auth");
 
 router.post("/signup", async (req, res) => {
-    const userSchema = Joi.object().keys({
-        email: Joi.string().required().email().regex(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/),
-        password: Joi.string().required().min(8).max(72),
-        phoneNumber: Joi.string().min(3).max(30).required(),
-        userFirstName: Joi.string().required(),
-        userLastName: Joi.string().required()
-    })
+  const userSchema = Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email()
+      .regex(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/),
+    password: Joi.string().required().min(8).max(72),
+    phoneNumber: Joi.string().min(3).max(30).required(),
+    userFirstName: Joi.string().required(),
+    userLastName: Joi.string().required(),
+  });
 
-    const body = {
-        email: req.body.email,
-        password: req.body.password,
-        userFirstName: req.body.userFirstName,
-        userLastName: req.body.userLastName,
-        phoneNumber: req.body.phoneNumber,
-    }
+  const body = {
+    email: req.body.email,
+    password: req.body.password,
+    userFirstName: req.body.userFirstName,
+    userLastName: req.body.userLastName,
+    phoneNumber: req.body.phoneNumber,
+  };
 
-    //userSchema.validate(body)
+  //userSchema.validate(body)
 
-    const result = userSchema.validate(body)
+  const result = userSchema.validate(body);
 
-    const { error } = result;
-    const valid = error == null;
+  const { error } = result;
+  const valid = error == null;
 
-    if (!valid) {
-        res.status(422).json({
-            message: 'Invalid request',
-            data: body
-        })
-    }
+  if (!valid) {
+    res.status(422).json({
+      message: "Invalid request",
+      data: body,
+    });
+  }
 
-    let user = await User.findOne({ email: req.body.email })
-    if (user) {
-        return res.status(400).json({
-            msg: "Email already used !"
-        })
-    }
-    user = await User.findOne({ phoneNumber: req.body.phoneNumber })
-    if (user) {
-        return res.status(400).json({
-            msg: "User phone number already used !"
-        })
-    }
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({
+      msg: "Email already used !",
+    });
+  }
+  user = await User.findOne({ phoneNumber: req.body.phoneNumber });
+  if (user) {
+    return res.status(400).json({
+      msg: "User phone number already used !",
+    });
+  }
 
-    try {
-        user = new User({
-            email: req.body.email,
-            password: req.body.password,
-            phoneNumber: req.body.phoneNumber,
-            userFirstName: req.body.userFirstName,
-            userLastName: req.body.userLastName
-        })
+  try {
+    user = new User({
+      email: req.body.email,
+      password: req.body.password,
+      phoneNumber: req.body.phoneNumber,
+      userFirstName: req.body.userFirstName,
+      userLastName: req.body.userLastName,
+    });
 
-        const salt = await bcrypt.genSalt(10)
-        user.password = await bcrypt.hash(user.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-        await user.save()
+    await user.save();
 
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
 
-        jwt.sign(
-            payload,
-            "randomString", {
-            expiresIn: 10000
-        },
-            (err, token) => {
-                if (err) throw err
-                res.status(200).json({
-                    token,
-                    user
-                })
-            }
-        )
-    } catch (err) {
-        console.log(err.message)
-        res.status(500).send("Error in Saving")
-    }
-}
-)
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: 10000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+          user,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Error in Saving");
+  }
+});
 
 router.post("/login", async (req, res) => {
-    const userSchema = Joi.object().keys({
-        email: Joi.string().required().email().regex(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/),
-        password: Joi.string().required().min(8).max(72),
-    })
+  const userSchema = Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email()
+      .regex(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/),
+    password: Joi.string().required().min(8).max(72),
+  });
 
-    const body = {
-        email: req.body.email,
-        password: req.body.password
-    }
+  const body = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    const result = userSchema.validate(body);
+  const result = userSchema.validate(body);
 
-    const { error } = result;
-    const valid = error == null;
+  const { error } = result;
+  const valid = error == null;
 
-    if (!valid) {
-        res.status(422).json({
-            message: 'Invalid request',
-            data: body
-        })
-    }
+  if (!valid) {
+    res.status(422).json({
+      message: "Invalid request",
+      data: body,
+    });
+  }
 
-    try {
-        let user = await User.findOne({
-            'email': req.body.email
-        })
-        if (!user)
-            return res.status(400).json({
-                message: "user Not Exist"
-            })
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+    if (!user)
+      return res.status(400).json({
+        message: "user Not Exist",
+      });
 
-        const isMatch = await bcrypt.compare(req.body.password, user.password)
-        if (!isMatch)
-            return res.status(400).json({
-                message: "Incorrect Password !"
-            })
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch)
+      return res.status(400).json({
+        message: "Incorrect Password !",
+      });
 
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
 
-        jwt.sign(
-            payload,
-            "secret",
-            {
-                expiresIn: 3600
-            },
-            (err, token) => {
-                if (err) throw err
-                res.status(200).json({
-                    token,
-                })
-            }
-        )
-    } catch (e) {
-        console.error(e)
-        res.status(500).json({
-            message: "Server Error"
-        })
-    }
-}
-)
+    jwt.sign(
+      payload,
+      "secret",
+      {
+        expiresIn: 3600,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+        });
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
 
 router.get("/profile", auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id)
-        res.json(user);
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-})
+  try {
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
+// GET Request to restore list of Reservations taken by user
+router.get("/takenby/:id", auth, async (req, res) => {
+  try {
+    const takenByUser = await Reservation.find({
+      reservationBy: req.user.id,
+    })
+      .populate("Item")
+      .populate("User");
+    res.json({
+      takenByUser,
+      message: "all items taken by user are successfully sent",
+    });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 module.exports = router;
